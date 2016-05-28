d36e8e4c8d8bc24431c525484d17489a07ecf3e0
cjc <1715542041@qq.com>
2016-05-28 11:55:11 +0800

    init DB

---
+++ src/main/java/ivo_chuanzhi/coolweather/db/CoolweatherOpenHelper.java
@@ -0,0 +1,58 @@
+package ivo_chuanzhi.coolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by chenjiacheng on 2016/5/28.
+ */
+
+public class CoolweatherOpenHelper extends SQLiteOpenHelper{
+
+    /**
+     * Provicne表的建表语句
+     */
+    public static final String CREATE_PROVINCE = "create table Province ("
+            + "id integer primary key autoincrement, "
+            + "province_name text, "
+            + "province_code text)";
+
+    /**
+     * City表的建表语句
+     */
+    public static final String CREATE_CITY = "create table City ("
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer)";
+
+    /**
+     * County表的建表语句
+     */
+    public static final String CREATE_COUNTY = "create table County ("
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer)";
+
+
+
+    public CoolweatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE); // 省
+        db.execSQL(CREATE_CITY); // 市
+        db.execSQL(CREATE_COUNTY); // 县
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+}
---
+++ src/main/java/ivo_chuanzhi/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package ivo_chuanzhi.coolweather.model;
+
+/**
+ * Created by chenjiacheng on 2016/5/28.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
